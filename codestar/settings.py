fcf7f40605d683be217eb3ffa13a3af3d07fee65
Laura Read <laurajmtb@gmail.com>
2024-09-06 10:23:11 +0000

    Debug False

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-ljtalks-djangoblog-vxwf77jmvgl.ws.codeinstitute-ide.net", '.herokuapp.com']
 
