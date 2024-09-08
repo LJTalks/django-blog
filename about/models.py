3accd45f61898aeb158a57504fd9194e93e7464d
Laura Read <laurajmtb@gmail.com>
2024-09-08 19:03:39 +0000

    Trying to fix image links

--- about/models.py
+++ about/models.py
@@ -6,9 +6,10 @@ from cloudinary.models import CloudinaryField
 
 class About(models.Model):
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    profile_image = CloudinaryField('image', default='placeholder')
+
 
     def __str__(self):
         return self.title
